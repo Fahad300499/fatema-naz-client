f5a629eb255b9f33737d63ced1242983d4e6a8dc
Fahad300499 <fahad.147448@gmail.com>
2026-02-27 22:20:55 +0600

    updated

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router";
-import { AuthContext } from "../Pages/providers/AuthContext";
+import { AuthContext } from "../Pages/Providers/AuthContext";
 
 
 const PrivateRoute = ({ children }) => {
